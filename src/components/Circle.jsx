5f6da46cbdc535bd538d8715928953ca4cad3675
zack-jack <zjackson@redventures.com>
2021-06-06 20:38:47 -0400

    static render for game step 2

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-const Circle = ({ color, icon }) => (
-  <div className={`circle circle--${color}`}>
-    <div className="circle__outer">
-      <div className="circle__inner">
-        {icon}
+const Circle = ({ color, empty, icon }) => {
+  if (empty) {
+    return (
+      <div className="circle circle--empty">
+        <div className="circle__inner" />
+      </div>
+    );
+  }
+
+  return (
+    <div className={`circle circle--${color}`}>
+      <div className="circle__outer">
+        <div className="circle__inner">
+          {icon}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+Circle.defaultProps = {
+  color: '',
+  empty: false,
+  icon: null,
+};
 
 Circle.propTypes = {
-  color: PropTypes.string.isRequired,
-  icon: PropTypes.node.isRequired,
+  color: PropTypes.string,
+  empty: PropTypes.bool,
+  icon: PropTypes.node,
 };
 
 export default Circle;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as ScissorsSVG } from '../assets/images/_icon-scissors.s
 
 const Game = () => {
   const [selectedValue, setSelectedValue] = useState('');
+  const [compValue, setCompValue] = useState('');
 
   const submit = (e) => {
     e.preventDefault();
@@ -17,8 +18,39 @@ const Game = () => {
     submit(e);
   };
 
+  if (selectedValue && compValue) {
+    // Win, loss, tie declared
+    return (
+      <div>
+        outcome
+      </div>
+    );
+  }
+
+  if (selectedValue) {
+    // User selection made, computer randomizing value
+    return (
+      <div className="game step-two">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col items-center">
+            <Circle
+              color="blue"
+              icon={<PaperSVG />}
+            />
+            <p className="game__selection-label mt-4">You picked</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Circle empty />
+            <p className="game__selection-label mt-4">The house picked</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // No user selection has been made yet
   return (
-    <div className="game">
+    <div className="game step-one">
       <form noValidate className="game__form" onSubmit={submit}>
         <InputRadio
           formValue="paper"
